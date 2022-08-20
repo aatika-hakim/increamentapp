688160a6fd85bf54bbd4bebbd0ef7c080fe4651b
Aatika Hakim <aatikahakim90@gmail.com>
2022-08-20 12:05:02 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+
+  const[num, setNum] = useState(0);
+
+  const incNum = () => {
+    setNum(num + 1);
+  };
+
+  const decNum = () => {
+    if(num > 0){
+      setNum(num - 1);
+    }else{
+      setNum(0);
+    }
+    
+  };
+
+  return(
+    <>
+      <div className="container">
+        <div className="wrapper">
+        <h1> {num} </h1>
+        <div className="btn">
+        <button onClick={incNum}> Increment </button>
+        <button onClick={decNum}> Decrement </button>
+        </div>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
